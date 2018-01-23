a8168cbf831106796472ea7df726d031dd371b85
HomieRicky <ricardoferrsilva@hotmail.com>
2018-01-23 16:45:39 -0500

    finished scheduling thing

--- install_packages.bat
+++ install_packages.bat
@@ -2,6 +2,7 @@ pip install moviepy
 pip install pytube
 pip install beautifulsoup4
 pip install twython
+pip install pause
 python - <<EOF
 import imageio.plugins.ffmpeg
 imageio.plugins.ffmpeg.download()
--- run_schedule.py
+++ run_schedule.py
@@ -1,6 +1,7 @@
 import pause
 from datetime import datetime
 from datetime import timedelta
+import os
 
 def round_to_next_hour(date):
     date += timedelta(seconds=60-date.second)
@@ -14,8 +15,18 @@ def time_str(date):
 date = datetime.now()
 print('Current time is ' + time_str(date))
 
-date = round_to_next_hour(date)
-print('Next run is at ' + time_str(date))
+failure_limit = 3
+dir = os.path.dirname(os.path.realpath(__file__))
 
-#while True:
-#    pause.until(datetime())
+while True:
+    date = round_to_next_hour(date)
+    print('Next run is at ' + time_str(date))
+    dt = datetime(date.year, date.month, date.day, date.hour, date.minute, date.second, 0).timestamp()
+    pause.until(dt)
+    fails = 0
+    while fails < 3 and os.system('python ' + dir + '\gotmoon.py') != 0:
+        fails += 1
+    if fails == 3:
+        print('Program failed 3 times at ' + time_str(date))
+        break
+
